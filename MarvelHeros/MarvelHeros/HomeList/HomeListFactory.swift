290d76526c5a32183189aa37ebc241d3ba284824
Thiago Santos <t.vaz.santos@gmail.com>
2022-05-06 23:10:21 -0300

    Integrando com Service

--- MarvelHeros/MarvelHeros/HomeList/HomeListFactory.swift
+++ MarvelHeros/MarvelHeros/HomeList/HomeListFactory.swift
@@ -10,7 +10,10 @@ import UIKit
 enum HomeListFactory {
     
     static func make(wireframe: HomeListWireframe) -> HomeViewController {
-        let interactor = HomeListInteractor()
+        let apiClient =  MarvelApiClient(publicKey: "650e801e1408159969078d2a1361c71c", privateKey: "20112b45612fd05f0d21d80d70bc8c971695c7f1")
+
+        let manager = MarvelManager(client: apiClient)
+        let interactor = HomeListInteractor(manager: manager, total: 1559)
         let presenter = HomeListPresenter(interactor: interactor, router: wireframe)
         interactor.output = presenter
         let viewController = HomeViewController(presenter: presenter)
--- MarvelHeros/MarvelHeros/HomeList/HomeListInteractor.swift
+++ MarvelHeros/MarvelHeros/HomeList/HomeListInteractor.swift
@@ -16,9 +16,29 @@ protocol HomeListInteractorOutput: AnyObject {
 }
 
 class HomeListInteractor: HomeListInteracting {
+    let total: Int
+    var offset: Int = 0
+    var count: Int  = 0
+    var limit: Int = 20
+    var name: String? = nil
+    
+    let manager: MarvelManager
+    
     weak var output:HomeListInteractorOutput?
     
+    init(manager: MarvelManager, total: Int) {
+        self.manager = manager
+        self.total = total
+    }
+    
     func loadService() {
-        print("carregar os dados")
+        manager.getCharacters(GetCharacters(name: name, limit: limit, offset: offset)) { [weak self] result in
+            switch result {
+            case let .success(data):
+                print(data)
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
 }
